b73e0e0b2049afbe1afe761801368d4fbdc6210c
AVMihel <beglec2010@gmail.com>
2025-04-06 14:24:13 +0500

    Add test

---
+++ src/test/java/ru/netology/rest/PostmanApiTest.java
@@ -0,0 +1,28 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanApiTest {
+
+    @Test
+    void shouldReturnPostedData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
+
+
